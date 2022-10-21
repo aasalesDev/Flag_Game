2b4f18d6f9103c1db7dc9a8c585dac93b3c1e707
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-21 09:44:58 -0300

    added player's score in the nav bar, kept track of quantity of questions asked, and popped up a message indicated made mistakes

--- FlagGame/ViewController.swift
+++ FlagGame/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     @IBOutlet weak var buttonThree: UIButton!
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,24 +29,25 @@ class ViewController: UIViewController {
          buttonOne.layer.borderWidth = 1
          buttonTwo.layer.borderWidth = 1
          buttonThree.layer.borderWidth = 1
-        
+         
          buttonOne.layer.borderColor = UIColor.lightGray.cgColor
          buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
          buttonThree.layer.borderColor = UIColor.lightGray.cgColor
          */
-                
+        
         askQuestion(action: nil)
     }
     
     func askQuestion(action: UIAlertAction!){
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        questionsAsked += 1
         
         buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
         buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
         buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        title = "\(countries[correctAnswer].uppercased()) | Score: \(score)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -56,15 +58,31 @@ class ViewController: UIViewController {
             score += 1
         } else {
             title = "Incorrect"
-            score -= 1
+            //score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        
-        present(ac, animated: true)
+        if questionsAsked < 10{
+            if title == "Incorrect" {
+                let ac = UIAlertController(title: title, message: "You should have chosen row \(correctAnswer+1)", preferredStyle: .alert)
+                
+                ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+                
+                present(ac, animated: true)
+            } else {
+                let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+                
+                ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+                
+                present(ac, animated: true)
+            }
+        } else {
+            let ac = UIAlertController(title: "Wow", message: "You have already answered \(questionsAsked) questions! That's enough!", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+
+            present(ac, animated: true)
+        }
     }
-    
 }
+
 
